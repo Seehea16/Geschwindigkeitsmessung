746a2f71a39bc6bd07e23a396787d01d0777452d
Herbert Seewann <seehea16@htl-kaindorf.ac.at>
2018-10-24 15:02:21 +0200

    Measurement v2 / DateTime formattieren

--- src/Geschwindigkeitsmessung/Measurement.java
+++ src/Geschwindigkeitsmessung/Measurement.java
@@ -3,8 +3,13 @@ package Geschwindigkeitsmessung;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Measurement implements Serializable{
+    public static final DateTimeFormatter DTFDATE = DateTimeFormatter.
+            ofPattern("dd.MM.yyyy");
+    public static final DateTimeFormatter DTFTIME = DateTimeFormatter.
+            ofPattern("HH:mm");
     private LocalDate date;
     private LocalTime time;
     private String kennz;
@@ -43,5 +48,13 @@ public class Measurement implements Serializable{
 
     public int getUeber() {
         return ueber;
+    }
+    
+    public String getDateAsString() {
+        return this.date.format(DTFDATE);
+    }
+    
+    public String getTimeAsString() {
+        return this.time.format(DTFTIME);
     }
 }
