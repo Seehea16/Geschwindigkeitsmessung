c09de8becd85b00185750c431599e2594198bf15
Herbert Seewann <seehea16@htl-kaindorf.ac.at>
2018-10-24 19:22:41 +0200

    VelocityTableModell Klasse v1

--- src/Geschwindigkeitsmessung/VelocityTableModell.java
+++ src/Geschwindigkeitsmessung/VelocityTableModell.java
@@ -1,5 +1,30 @@
 package Geschwindigkeitsmessung;
 
-public class VelocityTableModell {
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
 
+public class VelocityTableModell extends AbstractTableModel {
+    private List<Measurement> liste = new ArrayList<>();
+    
+    @Override
+    public int getRowCount() {
+        return liste.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return MeasurementColumnEnum.values().length;
+    }
+
+    @Override
+    public String getColumnName(int columnIndex) {
+        return MeasurementColumnEnum.values()[columnIndex].getName();
+    }
+    
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Measurement m = liste.get(rowIndex);
+        return m;
+    }
 }
