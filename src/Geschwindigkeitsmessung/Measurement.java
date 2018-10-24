84a630142a28172e161c828e5becbef1aed2d88c
Herbert Seewann <seehea16@htl-kaindorf.ac.at>
2018-10-24 11:34:55 +0200

    Measurement - Klasse v1

--- src/Geschwindigkeitsmessung/Measurement.java
+++ src/Geschwindigkeitsmessung/Measurement.java
@@ -1,5 +1,47 @@
 package Geschwindigkeitsmessung;
 
-public class Measurement {
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
+public class Measurement implements Serializable{
+    private LocalDate date;
+    private LocalTime time;
+    private String kennz;
+    private int geschw;
+    private int erl;
+    private int ueber;
+
+    public Measurement(LocalDate date, LocalTime time, String kennz, int geschw, int erl) {
+        this.date = date;
+        this.time = time;
+        this.kennz = kennz;
+        this.geschw = geschw;
+        this.erl = erl;
+        this.ueber = ueber - erl;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public String getKennz() {
+        return kennz;
+    }
+
+    public int getGeschw() {
+        return geschw;
+    }
+
+    public int getErl() {
+        return erl;
+    }
+
+    public int getUeber() {
+        return ueber;
+    }
 }
